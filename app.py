e64dd40918f40ecdf3d2a2006ca59bbc8c35ad1c
Rose Grundy <roseanna@google.com>
2017-04-11 18:43:24 +0100

    add log statement

--- app.py
+++ app.py
@@ -19,12 +19,13 @@ from flask import make_response
 # Flask app should start in global layout
 app = Flask(__name__)
 
-
 @app.route('/webhook', methods=['POST'])
+
 def webhook():
     req = request.get_json(silent=True, force=True)
-    print("Headers:")
-    print(request.headers)
+
+    authorization = request.authorization
+    print(authorization)
 
     print("Request:")
     print(json.dumps(req, indent=4))
